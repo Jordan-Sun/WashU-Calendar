2f3ca46745555fc313708990fd9371fef4617065
Jordan <sk.jordan@foxmail.com>
2020-07-17 15:13:19 -0500

    Initial Commit

---
+++ WashU Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WashU Calendar
+//
+//  Created by Zhuoran Sun on 2020/7/17.
+//  Copyright © 2020 washu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
