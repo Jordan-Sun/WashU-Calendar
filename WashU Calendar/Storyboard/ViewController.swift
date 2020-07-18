c68807d702eee0ad29d6889c0c4f246ee998783d
Jordan <sk.jordan@foxmail.com>
2020-07-18 17:24:33 -0500

    - Switched data model to codeine. - Added collection view and applied diffable data source.

--- WashU Calendar/Storyboard/ViewController.swift
+++ WashU Calendar/Storyboard/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
-    private var appDelegate = UIApplication.shared.delegate as!  AppDelegate
-    private var context = (UIApplication.shared.delegate as!  AppDelegate).persistentContainer.viewContext
     
+    // Core Data Component
+    /// App delegate
+    private var appDelegate = UIApplication.shared.delegate as!  AppDelegate
+    /// Core data context
+    private var context = (UIApplication.shared.delegate as!  AppDelegate).persistentContainer.viewContext
+    /// Core data fetched result controller
+    private var eventFetchedResultController: NSFetchedResultsController<Event>!
+    
+    // Collection View Component
+    /// Collection view
+    @IBOutlet weak var eventCollectionView: UICollectionView!
+    /// Collection view data source
+    private var eventCollectionViewDiffableDataSource: UICollectionViewDiffableDataSource<Section,Event>!
+    
+    // Debug Component
+    /// An array that temporarily stores courses
     var courses = [Course]()
+    /// Main section of the collection view
+    enum Section {
+        case main
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureCollectionDataSource()
+        configureCollectionLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -34,12 +54,173 @@ class ViewController: UIViewController {
             let nserror = error as NSError
             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
+        updateSnapshot()
     }
     
     //Debug functions
     
-    @IBAction func addRandomCourse(_ sender: Any) {
-        addCourseToCoreData(name: "Test Course", id: "000")
+    @IBAction func addTenRandomCourse(_ sender: Any) {
+        for i in 1 ... 10 {
+            let newCourse = addCourseToCoreData(name: "Test Course \(i)", id: "000")
+            for j in 1 ... 10 {
+                addEventToCoreData(name: "Test Course \(i) Event \(j)",interval: .init(start: Date(), duration: Double.random(in: 1.0 ... 10.0) * 60), to: newCourse)
+            }
+        }
+    }
+    
+}
+
+// Collection View Delegate
+
+//extension ViewController {
+//
+//}
+
+// Collection View Layout
+
+extension ViewController {
+    
+    /// Configure the layout of the movie preview collection view
+    private func configureCollectionLayout() {
+        eventCollectionView.collectionViewLayout = createLayout()
+    }
+    
+    /// Create a new collection view compositional layout
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        
+        let layout = UICollectionViewCompositionalLayout {
+            (_: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            
+            let groupColumns = 1
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: groupColumns)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            
+            return section
+            
+        }
+        
+        return layout
+        
+    }
+    
+}
+
+// Collection View Data Source
+
+extension ViewController: NSFetchedResultsControllerDelegate {
+    
+    /// Configure the datasource of the movie preview collection view
+    private func configureCollectionDataSource() {
+        
+        // Diffable data source cell provider
+        eventCollectionViewDiffableDataSource = UICollectionViewDiffableDataSource<Section,Event>(collectionView: self.eventCollectionView) { (collectionView, indexPath, event) -> UICollectionViewCell? in
+            
+            // Dequeue reuseable cell
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "eventCell", for: indexPath) as? EventCollectionViewCell else {
+                fatalError("Expected reused cell to be of type MovieCollectionViewCell.")
+            }
+            
+            // Update cell
+            cell.nameLabel.text = event.name
+            
+            return cell
+            
+        }
+        
+    }
+    
+    private func updateSnapshot() {
+        
+        // Fetch request
+        let request = Event.fetchRequest() as NSFetchRequest<Event>
+        let sort = NSSortDescriptor(key: #keyPath(Event.start), ascending: true)
+        request.sortDescriptors = [sort]
+        do {
+            eventFetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            try eventFetchedResultController.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        // Update snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section,Event>()
+        if let events = eventFetchedResultController.fetchedObjects {
+            snapshot.appendSections([.main])
+            snapshot.appendItems(events, toSection: .main)
+        }
+        eventCollectionViewDiffableDataSource.apply(snapshot)
+        
+    }
+    
+    
+    
+}
+
+// Core Data Interactions
+
+extension ViewController {
+    
+    /// Add a new course to core data.
+    /// - Parameters:
+    ///   - name: the name of the course. (required)
+    ///   - id: the id of the course. (required)
+    ///   - professor: the professor who teaches the course
+    ///   - department: the department of which the course belongs to.
+    ///   - session: the session of which the course belongs to.
+    ///   - attributes: an array of attributes that the course conforms to.
+    @discardableResult func addCourseToCoreData(name: String, id: String, professor: String? = nil, department: Department? = nil, session: Session? = nil, attributes: [Attribute]? = nil) -> Course {
+        let newCourse = Course(entity: Course.entity(), insertInto: context)
+        newCourse.name = name
+        newCourse.id = id
+        newCourse.professor = professor
+        newCourse.department = department
+        newCourse.session = session
+        if let attributes = attributes {
+            newCourse.addToAttributes(NSSet(array: attributes))
+        }
+        appDelegate.saveContext()
+        updateSnapshot()
+        return newCourse
+    }
+    
+    /// Add a new event to core data.
+    /// - Parameters:
+    ///   - start: the start time of the event.
+    ///   - end: the end time of the event.
+    ///   - location: the location where the event takes place.
+    ///   - course: the course of which the event belongs to.
+    @discardableResult func addEventToCoreData(name: String, from start: Date, to end: Date, to course: Course, at location: String? = nil) -> Event {
+        let newEvent = Event(entity: Event.entity(), insertInto: context)
+        newEvent.name = name
+        newEvent.start = start
+        newEvent.end = end
+        newEvent.location = location
+        newEvent.course = course
+        appDelegate.saveContext()
+        updateSnapshot()
+        return newEvent
+    }
+    
+    /// An alternative way to add a new event to core data.
+    @discardableResult func addEventToCoreData(name: String, interval: DateInterval, to course: Course, at location: String? = nil) -> Event {
+        let newEvent = Event(entity: Event.entity(), insertInto: context)
+        newEvent.name = name
+        newEvent.start = interval.start
+        newEvent.end = interval.end
+        newEvent.location = location
+        newEvent.course = course
+        appDelegate.saveContext()
+        updateSnapshot()
+        return newEvent
     }
     
 }
