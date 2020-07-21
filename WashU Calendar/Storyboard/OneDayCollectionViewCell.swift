81db2de5b5ed53fc46fe7ede5ccec58434f908a5
Coco Zhu <runqi.zhu@wustl.edu>
2020-07-20 19:13:36 -0700

    - added something in today view

---
+++ WashU Calendar/Storyboard/OneDayCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  OneDayCollectionViewCell.swift
+//  WashU Calendar
+//
+//  Created by Coco Zhu on 7/20/20.
+//  Copyright © 2020 washu. All rights reserved.
+//
+
+import UIKit
+
+class OneDayCollectionViewCell: UICollectionViewCell {
+    
+    var date: Date!
+    
+    @IBOutlet weak var theScrollView: UIScrollView!
+    
+}
--- WashU Calendar/Storyboard/TodayViewController.swift
+++ WashU Calendar/Storyboard/TodayViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import CoreData
 
-class TodayViewController: UIViewController {
+class TodayViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
 
+    let currentDate = Date()
+    
+    
+    @IBOutlet weak var theCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        theCollectionView.dataSource = self
+        theCollectionView.delegate = self
+        theCollectionView.register(OneDayCollectionViewCell.self, forCellWithReuseIdentifier: "oneDayCell")
+        setUpCollectionView()
+        
+//        let calendar = Calendar.current
+//        let hour = calendar.component(.hour, from: currentDate)
+//        let minutes = calendar.component(.minute, from: currentDate)
 
         // Do any additional setup after loading the view.
     }
+    
+    /// for now we just display one-day view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func setUpCollectionView() {
+        theCollectionView.collectionViewLayout = configureLayout()
+    }
+    
+    
+    /// following code is learned from video: https://www.hackingwithswift.com/read/10/2/designing-uicollectionview-cells
+    /// functionality: set up a one-day view cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = theCollectionView.dequeueReusableCell(withReuseIdentifier: "oneDayCell", for: indexPath) as! OneDayCollectionViewCell
+//        cell.theScrollView.backgroundColor = .systemPink
+        cell.date = currentDate
+        print(cell.date)
+
+            
+        return cell
+    }
+        
+        
+    /// following code is learned from video: https://www.youtube.com/watch?v=ZXupp2kOcSI
+    /// functionality: set up the layout of collection view
+        func configureLayout() -> UICollectionViewCompositionalLayout {
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(100))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+            let section = NSCollectionLayoutSection(group: group)
+            return UICollectionViewCompositionalLayout(section: section)
+
+        }
     
 
     /*
