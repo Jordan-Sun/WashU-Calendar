a5ed4e0c654087c477868f0d96aec91fff7dca5a
Jordan <sk.jordan@foxmail.com>
2020-07-30 04:42:34 -0500

    Changes

--- WashU Calendar Widget/WashU_Calendar_Widget.swift
+++ WashU Calendar Widget/WashU_Calendar_Widget.swift
@@ -9,6 +9,7 @@
 import WidgetKit
 import SwiftUI
 import Intents
+import CoreData
 
 struct WidgetEvent {
     let eventName: String
@@ -22,29 +23,132 @@ extension WidgetEvent {
 }
 
 struct Provider: IntentTimelineProvider {
+    
+    var managedObjectContext : NSManagedObjectContext
+    
     public func snapshot(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+//        // Initialize fetch request
+//        let request = Event.fetchRequest() as NSFetchRequest<Event>
+//
+//        // Apply predicate if date is not nil
+//        let calendar = Calendar.current
+//        let dayStart = calendar.startOfDay(for: Date()) as NSDate
+//        guard let dayEnd = calendar.date(byAdding: DateComponents(day: 1), to: dayStart as Date) as NSDate? else {
+//            fatalError("Fail to compute the end of day for date: \(dayStart).")
+//        }
+//        let predicate = NSPredicate(format: "(start >= %@) AND (start <= %@)", dayStart, dayEnd)
+//        request.predicate = predicate
+//
+//        // Apply sort descriptors
+//        let sort = NSSortDescriptor(key: #keyPath(Event.start), ascending: true)
+//        request.sortDescriptors = [sort]
+//
+//        // Try to perform fetch request
+//        var eventFetchedResultController: NSFetchedResultsController<Event>!
+//        do {
+//            eventFetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+//            try eventFetchedResultController.performFetch()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nserror = error as NSError
+//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//        }
+        
+        let widgetEvent = WidgetEvent(eventName: "Nothing to do", eventInterval: DateInterval(start: Date(), end: Date()), eventLocation: "Take a break", eventColor: .systemTeal)
+        let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+        
+//        // Generate a timeline.
+//        guard let event = eventFetchedResultController.fetchedObjects?.first else {
+//            let widgetEvent = WidgetEvent(eventName: "Nothing to do", eventInterval: DateInterval(start: Date(), end: Date()), eventLocation: "Take a break", eventColor: .systemTeal)
+//            let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+//
+//            completion(entry)
+//            return
+//        }
+//
+//        let widgetEvent = WidgetEvent(eventName: event.name ?? "Unknown Event", eventInterval: DateInterval(start: event.start!, end: event.end!), eventLocation: event.location, eventColor: event.color as! UIColor)
+//        let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+        
         completion(entry)
     }
-
+    
     public func timeline(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        
+//        // Initialize fetch request
+//        let request = Event.fetchRequest() as NSFetchRequest<Event>
+//
+//        // Apply predicate if date is not nil
+//        let calendar = Calendar.current
+//        let dayStart = calendar.startOfDay(for: Date()) as NSDate
+//        guard let dayEnd = calendar.date(byAdding: DateComponents(day: 1), to: dayStart as Date) as NSDate? else {
+//            fatalError("Fail to compute the end of day for date: \(dayStart).")
+//        }
+//        let predicate = NSPredicate(format: "(start >= %@) AND (start <= %@)", dayStart, dayEnd)
+//        request.predicate = predicate
+//
+//        // Apply sort descriptors
+//        let sort = NSSortDescriptor(key: #keyPath(Event.start), ascending: true)
+//        request.sortDescriptors = [sort]
+//
+//        // Try to perform fetch request
+//        var eventFetchedResultController: NSFetchedResultsController<Event>!
+//        do {
+//            eventFetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+//            try eventFetchedResultController.performFetch()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nserror = error as NSError
+//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//        }
+        
         var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        
+        // Generate a timeline.
+        let widgetEvent = WidgetEvent(eventName: "Nothing to do", eventInterval: DateInterval(start: Date(), end: Date()), eventLocation: "Take a break", eventColor: .systemTeal)
+        let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+        let calendar = Calendar.current
+        let timeline = Timeline(entries: [entry], policy: .after(calendar.startOfDay(for: Date().addingTimeInterval(24.0 * 60.0 * 60.0))))
         completion(timeline)
+//        guard let events = eventFetchedResultController.fetchedObjects else {
+//            let widgetEvent = WidgetEvent(eventName: "Nothing to do", eventInterval: DateInterval(start: Date(), end: Date()), eventLocation: "Take a break", eventColor: .systemTeal)
+//            let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+//
+//            let timeline = Timeline(entries: [entry], policy: .after(calendar.startOfDay(for: Date().addingTimeInterval(24.0 * 60.0 * 60.0))))
+//            completion(timeline)
+//            return
+//        }
+//        guard !events.isEmpty else {
+//            let widgetEvent = WidgetEvent(eventName: "Nothing to do", eventInterval: DateInterval(start: Date(), end: Date()), eventLocation: "Take a break", eventColor: .systemTeal)
+//            let entry = SimpleEntry(date: Date(), event: widgetEvent, configuration: configuration)
+//
+//            let timeline = Timeline(entries: [entry], policy: .after(calendar.startOfDay(for: Date().addingTimeInterval(24.0 * 60.0 * 60.0))))
+//            completion(timeline)
+//            return
+//        }
+//        var lastEnd = Date()
+//
+//        for event in events {
+//            let widgetEvent = WidgetEvent(eventName: event.name ?? "Unknown Event", eventInterval: DateInterval(start: event.start!, end: event.end!), eventLocation: event.location, eventColor: event.color as! UIColor)
+//            let entry = SimpleEntry(date: lastEnd, event: widgetEvent, configuration: configuration)
+//            lastEnd = widgetEvent.eventInterval.end
+//            entries.append(entry)
+//        }
+//        let timeline = Timeline(entries: entries, policy: .atEnd)
+//        completion(timeline)
     }
+    
+    func placeholder(with: Context) -> SimpleEntry {
+        let previewEvent = WidgetEvent.previewData
+        return SimpleEntry(date: Date(), event: previewEvent, configuration: ConfigurationIntent())
+    }
+    
 }
 
 struct SimpleEntry: TimelineEntry {
     public let date: Date
+    public let event: WidgetEvent
     public let configuration: ConfigurationIntent
 }
 
@@ -57,19 +161,21 @@ struct PlaceholderView : View {
 struct WashU_Calendar_WidgetEntryView : View {
     
     var entry: Provider.Entry
-    var event: WidgetEvent
     
     var body: some View {
         
         VStack(alignment: .leading) {
             
             DayView(entry: entry)
-            Text("in 10 minutes")
+            Spacer()
+            Text(entry.event.eventInterval.start, style: .time)
                 .font(.caption)
-            EventView(event: event)
+                .multilineTextAlignment(.leading)
+                .frame(width: 40.0, height: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+            EventView(event: entry.event)
             
         }
-        .padding(.all, 0.0)
+        .padding(.vertical, 8.0)
         
     }
 }
@@ -122,8 +228,8 @@ struct EventView: View {
                 .font(.caption)
                 .minimumScaleFactor(0.6)
         }
-        .padding(.all, 4.0)
-        
+        .padding(.vertical, 4.0)
+        .frame(width: /*@START_MENU_TOKEN@*/140.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/40.0/*@END_MENU_TOKEN@*/)
         .background(Color(event.eventColor))
         .cornerRadius(12)
         
@@ -134,19 +240,45 @@ struct EventView: View {
 @main
 struct WashU_Calendar_Widget: Widget {
     private let kind: String = "WashU_Calendar_Widget"
-
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentCloudKitContainer(name: "WashU_Calendar")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
     public var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(), placeholder: PlaceholderView()) { entry in
-            WashU_Calendar_WidgetEntryView(entry: entry, event: WidgetEvent.previewData)
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(managedObjectContext: persistentContainer.viewContext)) { entry in
+            WashU_Calendar_WidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Track Event")
+        .description("Keep track of one event.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
 struct WashU_Calendar_Widget_Previews: PreviewProvider {
     static var previews: some View {
-        WashU_Calendar_WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()), event: WidgetEvent.previewData)
+        WashU_Calendar_WidgetEntryView(entry: SimpleEntry(date: Date(), event: WidgetEvent.previewData, configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+        
+        WashU_Calendar_WidgetEntryView(entry: SimpleEntry(date: Date(), event: WidgetEvent.previewData, configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .redacted(reason: .placeholder)
     }
 }
